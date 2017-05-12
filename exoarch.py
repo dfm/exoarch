a045abbbbfe824ab39cc4813dbf680e4d99c87f5
Dan F-M <danfm@nyu.edu>
2017-05-12 09:41:07 -0700

    name kwarg

--- exoarch.py
+++ exoarch.py
@@ -84,8 +84,6 @@ class ExoplanetArchiveCatalog(Catalog):
     def __init__(self, name=None, **kwargs):
         if name is not None:
             self.name = name
-        else:
-            self.name = self.name
         super(ExoplanetArchiveCatalog, self).__init__(**kwargs)
 
     @property
@@ -93,11 +91,11 @@ class ExoplanetArchiveCatalog(Catalog):
         if self.name is None:
             raise NotImplementedError("subclasses must provide a name")
         return ("http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/"
-                "nph-nstedAPI?table={0}&select=*").format(self._name)
+                "nph-nstedAPI?table={0}&select=*").format(self.name)
 
     def _save_fetched_file(self, file_handle):
         df = pd.read_csv(file_handle)
-        df.to_hdf(self.filename, self._name, format="t")
+        df.to_hdf(self.filename, self.name, format="t")
 
 
 class KOICatalog(ExoplanetArchiveCatalog):
