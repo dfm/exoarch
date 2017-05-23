f6b43fddf62efb6a822cc705949c2f68cb356d21
Dan F-M <danfm@nyu.edu>
2017-05-23 13:10:45 -0700

    option to save catalogs as CSV

--- exoarch.py
+++ exoarch.py
@@ -81,21 +81,38 @@ class Catalog(object):
 
 class ExoplanetArchiveCatalog(Catalog):
 
-    def __init__(self, name=None, **kwargs):
+    def __init__(self, name=None, save_as_csv=False, **kwargs):
         if name is not None:
             self.name = name
+        self.save_as_csv = save_as_csv
+        if save_as_csv:
+            self.ext = ".csv"
         super(ExoplanetArchiveCatalog, self).__init__(**kwargs)
 
     @property
     def url(self):
         if self.name is None:
             raise NotImplementedError("subclasses must provide a name")
-        return ("http://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/"
+        return ("https://exoplanetarchive.ipac.caltech.edu/cgi-bin/nstedAPI/"
                 "nph-nstedAPI?table={0}&select=*").format(self.name)
 
     def _save_fetched_file(self, file_handle):
         df = pd.read_csv(file_handle)
-        df.to_hdf(self.filename, self.name, format="t")
+        if self.save_as_csv:
+            df.to_csv(self.filename, index=False)
+        else:
+            df.to_hdf(self.filename, self.name, format="t")
+
+    @property
+    def df(self):
+        if self._df is None:
+            if not os.path.exists(self.filename):
+                self.fetch()
+            if self.save_as_csv:
+                self._df = pd.read_csv(self.filename)
+            else:
+                self._df = pd.read_hdf(self.filename, self.name)
+        return self._df
 
 
 class KOICatalog(ExoplanetArchiveCatalog):
